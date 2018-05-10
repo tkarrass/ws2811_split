652b756ac6a06d50800c91b9f5ffdb92ea57c211
tkarrass <git@bitsetter.de>
2018-05-10 22:08:55 +0200

    some comments

--- stripes.c
+++ stripes.c
@@ -2,13 +2,31 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+volatile uint8_t port;
+
 int main(void) {
 	// init:
 	cli();
+	// port out
+	 
+	port = 1
+
+	// pin in
+	
+	// timer:
+	TCCR1A= 0;
+	TCCR1B=14;
+	OCR1A=0x0707;
+	TIMSK=64;
+
 	sei();
 	
 	// block.
 	// everything will be handled within the interrupt
 	while(1);
 	return 0; // mandatory for c, never reached in reality
+}
+
+ISR(TIMER1_COMPA_vect) {
+	
 }
